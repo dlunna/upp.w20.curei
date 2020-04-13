4886f6691bb7ecaaf0c267b9d7af16ef8083a4ac
dlunna <dlunna@gmail.com>
2020-04-13 14:48:06 -0500

    Creación de partado covi

---
+++ index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <?php include ('./dotk/00head/analitycs.html'); ?>
+  <?php include ('./dotk/00head/cabecera.html'); ?>
+  <?php include ('./dotk/00head/estilos.html'); ?>
+</head>
+
+<body>
+
+  <a id="arriba"></a>
+  <header>
+    <!-- Logos e imagen de hidalgo -->
+    <?php include ('../lib18//seph/hf/header.html'); ?>
+
+    <nav>
+      <!-- Menu principal superior -->
+      <?php //include ('../lib18//coreFRONTx/01header/nav/menu-superior/menu.html'); ?>
+    </nav>
+
+  </header>
+
+  <?php include ('./dotk/01header/02begajoso/menu-pegajoso.html'); ?>
+
+  <main>
+    <div class="container marketing">
+
+      <br />
+
+      <div class="text-center">
+            <img src="./mc/logo-curei.jpg" alt="CUREI" class="img-fluid" >
+      </div>
+
+      <h1>CLÍNICA UNIVERSITARIA DE REHABILITACIÓN INTEGRAL (C.U.Re.I)</h1>
+
+      <h2>Misión</h2>
+      <p>Somos una clínica de rehabilitación  que brinda atención y servicios de salud al público en general, enfocados a la prevención, tratamiento y rehabilitación de personas con limitaciones físicas, funcionales e intelectuales, para impulsar la recuperación, potenciación y mantenimiento de sus capacidades, e integrar a la persona de manera interpersonal, laboral y social mejorando su calidad de vida.</p>
+
+      <h2>Visión</h2>
+      <p>Convertirse en referente para las Universidades Politécnicas de nuestro país que oferten la Licenciatura en Terapia Física en la atención de pacientes y en la preparación del futuro de los egresados de nuestra Institución.</p>
+
+      <h1>Acerca de</h1>
+
+      <p>La Clínica Universitaria de Rehabilitación Integral se encuentra en la Universidad Politécnica de Pachuca y presta servicios de medicina enfocada al primer nivel de atención.</p>
+      <p>Los servicios de esta Clínica están dirigidos a estudiantes, docentes, administrativos y al público en general, a quienes se ofrece atención de consulta general y de especialidad, consulta de nutrición y terapia física.</p>
+      <p>C.U.Re.I. cuenta con espacio para consultorio, área de mecanoterapia, circuito de marcha al aire libre, área de agentes físicos, área de hidroterapia, sala de espera y recepción. Cuenta con equipo necesario para brindar dichos servicios.</p>
+      <p>Todas las consultas son atendidas por profesionales titulados altamente preparados y capacitados para evaluar, prevenir y establecer tratamientos de precisión, global, especializado e individualizado, promoviendo la calidad de vida de cada uno de nuestros usuarios.</p>
+      <p>Cada paciente da su consentimiento para que 2 o 3 estudiantes de la Licenciatura en Terapia Física puedan hacer sus observaciones y prácticas asignadas por docentes del programa educativo. Además de contarse con estudiantes de Estadía que realizan sus prácticas profesionales en la Clínica.</p>
+      <p>Nuestro sistema consultorio-laboratorio permite que el usuario y la práctica estudiantil se puedan llevar a cabo de forma profesional, ya que los estudiantes están supervisados por los médicos y licenciados en turno.</p>
+
+      <div class="container">
+
+        <div class="row">
+          <div class="col-sm-3">
+            <a href="./mc/1.jpg">
+              <img src="./mc/1.jpg" alt="curei" class="img-fluid img-thumbnail">
+            </a>
+          </div>
+          <div class="col-sm-3">
+            <a href="./mc/2.jpg">
+              <img src="./mc/2.jpg" alt="curei" class="img-fluid img-thumbnail">
+            </a>
+          </div>
+          <div class="col-sm-3">
+            <a href="./mc/3.jpg">
+              <img src="./mc/3.jpg" alt="curei" class="img-fluid img-thumbnail">
+            </a>
+          </div>
+          <div class="col-sm-3">
+            <a href="./mc/4.jpg">
+              <img src="./mc/4.jpg" alt="curei" class="img-fluid img-thumbnail">
+            </a>
+          </div>
+        </div>
+
+      </div>
+      <br>
+      <br>
+
+      <h1>Servicios</h1>
+
+      <ol>
+        <li>Electroterapia</li>
+        <li>Mecanoterapia</li>
+        <li>Masoterapia</li>
+        <li>Hidroterapia</li>
+        <li>Kinesioterapia</li>
+        <li>Laserterapia</li>
+        <li>Termoterapia</li>
+        <li>Ultrasonoterapia</li>
+      </ol>
+
+    <h1>Consulta de Medicina General y Especialidad en Geriatría, Consulta de Nutrición</h1>
+
+    <p>El servicio de Nutrición ofertado en el C.U.Re.I se encuentra a cargo del L.N. Erick Alexis Sánchez Tapia, quien se encarga de evaluar la composición corporal de los pacientes mediante la toma de antropometría (peso, talla, circunferencia de cintura), con el objetivo de brindar una atención integral a los solicitantes, en un trabajo interdisciplinario con los servicios de medicina y rehabilitación también ofertados; una vez establecido el diagnóstico corporal, se realiza la toma del historial clínico detallado de cada paciente y con base en ello se plantea un programa de alimentación individualizado que habrá de llevarse a cabo por un lapso de 4 semanas, además de que, durante la consulta nutricional, se brinda información y orientación alimentaria así como promoción de la salud a cada paciente.</p>
+    <p>Dentro de la consulta subsecuente se realiza de nuevo la valoración antropométrica para conocer los resultados de la intervención e integrar y proponer nuevas estrategias para el cumplimiento del objetivo nutricional de cada paciente.</p>
+
+    <div class="container">
+
+      <div class="row">
+        <div class="col-sm-3">
+          <a href="./mc/5.jpg">
+            <img src="./mc/5.jpg" alt="curei" class="img-fluid img-thumbnail">
+          </a>
+        </div>
+        <div class="col-sm-3">
+          <a href="./mc/6.jpg">
+            <img src="./mc/6.jpg" alt="curei" class="img-fluid img-thumbnail">
+          </a>
+        </div>
+        <div class="col-sm-3">
+          <a href="./mc/7.jpg">
+            <img src="./mc/7.jpg" alt="curei" class="img-fluid img-thumbnail">
+          </a>
+        </div>
+        <div class="col-sm-3">
+          <a href="./mc/logo-terapia.jpg">
+            <img src="./mc/logo-terapia.jpg" alt="curei" class="img-fluid img-thumbnail">
+          </a>
+        </div>
+      </div>
+
+    </div>
+
+    <br>
+    <br>
+
+    <h1>Ofrecemos Tratamiento fisioterapéutico para:</h1>
+
+    <ul>
+      <li>Acondicionamiento físico</li>
+      <li>Artritis</li>
+      <li>Cervicalgias</li>
+      <li>Ciatalgias</li>
+      <li>Ciática</li>
+      <li>Desgarros musculares</li>
+      <li>Dolores musculares</li>
+      <li>Dorsalgias </li>
+      <li>Escoliosis</li>
+      <li>Esguinces</li>
+      <li>Estimulación temprana</li>
+      <li>Fisioterapia en embarazadas</li>
+      <li>Fisioterapia postparto</li>
+      <li>Hernia de disco</li>
+      <li>Lesionado medular</li>
+      <li>Lesiones de hombro, codo y muñeca</li>
+      <li>Parálisis cerebral</li>
+      <li>Parálisis facial</li>
+      <li>Lumbalgia</li>
+      <li>Luxaciones</li>
+      <li>Rehabilitación cardiaca</li>
+      <li>Rehabilitación respiratoria</li>
+      <li>Reeducación de la marcha </li>
+      <li>Retraso psicomotor</li>
+      <li>Alteraciones del pie (plano, cavo, dedos en garra, etcétera)</li>
+      <li>Prevención de lesiones</li>
+      <li>Retraso psicomotor</li>
+      <li>Terapia manual</li>
+      <li>Trastornos neurológicos</li>
+      <li>Tratamiento postoperatorio</li>
+      <li>Túnel de carpo</li>
+      <li>Valoración de postura</li>
+      <li>Vendaje neuromuscular</li>
+      <li>Rehabilitación de fracturas</li>
+    </ul>
+
+    <h1>Horarios:</h1>
+
+    <p>Nuestros horarios de atención son de 8:00 a 16:00 horas de lunes a viernes, excepto días de suspensión oficial y días de Consejo Técnico Escolar para Educación Superior (CTEES).</p>
+
+    <h1>Costos</h1>
+
+    <p>Las consultas tienen un costo accesible (cuota de recuperación aproximada de $54.00) que debe realizarse en la Caja Institucional. </p>
+
+    <h1>Contacto</h1>
+
+    <p>
+      M.D.M. Laura Luz Ortega Cruz<br>
+      Responsable administrativa de la Clínica<br>
+      Carretera Pachuca-Ciudad Sahagún km 20, Rancho Luna, Ex Hacienda de Santa Bárbara, Municipio de Zempoala, Hidalgo. C.P. 43830<br>
+      Correo electrónico: curei@upp.edu.mx<br>
+      Teléfono (771) 547 7510 Ext. 2441
+</p>
+
+<!--
+        <p>Si requieres apoyo:</p>
+        <ul>
+          <li>Materias y calificaciones:
+            <ol>
+              <li>Lic. en Terapia Física: <a href="mailto:drachio@upp.edu.mx">drachio@upp.edu.mx</a></li>
+              <li>Ingenierías: <a href="mailto:mtorres@upp.edu.mx">mtorres@upp.edu.mx</a></li>
+              <li><a href="mailto: divisiondeestudios@upp.edu.mx">divisiondeestudios@upp.edu.mx</a></li>
+            </ol>
+          </li>
+          <li>Problemas para ingresar: <a href="mailto:soporte_tecnico@upp.edu.mx">soporte_tecnico@upp.edu.mx</a></li>
+          <li>Soporte técnico en general: <a href="mailto:sistemas@upp.edu.mx">sistemas@upp.edu.mx</a></li>
+        </ul>
+  -->
+
+
+
+
+      <br />
+      <br />
+      <a id="mapadesitio"></a>
+      <hr class="featurette-divider">
+      <!-- Menu con todos los sitios y mini sitios de la página -->
+      <?php include ('../lib18//coreFRONTx/main/tmenu-footer/menufooter.php'); ?>
+
+    </div>
+
+  </main>
+
+  <footer>
+    <!-- footer de gobierno del estado -->
+    <?php include ('../lib18//seph/hf/footer.html'); ?>
+  </footer>
+
+  <?php include ('../lib18//coreFRONTx/09js/java.html'); ?>
+
+  <!-- Script para dar la animacion al boton flotante que lleva arriba -->
+  <a href="#" class="back-to-top">Volver arriba</a>
+  <script src="../lib18//coreFRONTx/10boton-arriba/script.js"></script>
+
+</body>
+
+</html>
